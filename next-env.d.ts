1e67e1a88bd9c1d217d625f25e098ed593b697eb
habbswiss <habbswiss@gmail.com>
2026-04-29 10:56:35 +0200

    Add new partner logo for HABB Global in Partners component

---
+++ next-env.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// Diese Datei stellt sicher, dass TypeScript die von Next.js bereitgestellten Typdefinitionen
+// (insbesondere für `next/image`) korrekt lädt.
+
